0ef370ab8ceb64ce5b9b9c526287a181a4fced12
Mark de Jong <mark@markdejong.com>
2015-02-03 15:34:28 +0100

    Added npm to PATH

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,2 @@
-export PATH="$HOME/.jenv/bin:./bin:/usr/local/bin:/usr/local/sbin:$ZSHDOT/bin:$PATH:$M2"
+export PATH="/usr/local/share/npm/bin/:./bin:/usr/local/bin:/usr/local/sbin:$ZSHDOT/bin:$PATH:$M2"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
