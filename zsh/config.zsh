2b9ad63909248302148bbdbe0f485069dfde322f
Mark de Jong <mark@markdejong.com>
2015-02-03 15:50:12 +0100

    Added custom ZSH theme `mister`

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -5,7 +5,7 @@ export ZSH=$HOME/.oh-my-zsh
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
-ZSH_THEME="steeef"
+ZSH_THEME="mister"
 
 # Uncomment the following line to use case-sensitive completion.
 # CASE_SENSITIVE="true"
@@ -45,7 +45,7 @@ COMPLETION_WAITING_DOTS="true"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git mvn svn npm jekyll command-not-found rsync sudo brew bower atom screen z history-substring-search)
+plugins=(git mvn svn npm jekyll command-not-found rsync sudo brew bower atom screen z history-substring-search zsh-syntax-highlighting)
 
 # User configuration
 
